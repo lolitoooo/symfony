10ddb759e973babe835580794c01f921112efebd
lolitoooo <loanpena77@gmail.com>
2024-10-04 14:14:07 +0200

    Add Entity (Cooment, Language, User)

---
+++ src/Entity/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\UserAccountStatusEnum;
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: Types::SIMPLE_ARRAY, enumType: UserAccountStatusEnum::class)]
+    private array $account_status = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return UserAccountStatusEnum[]
+     */
+    public function getAccountStatus(): array
+    {
+        return $this->account_status;
+    }
+
+    public function setAccountStatus(array $account_status): static
+    {
+        $this->account_status = $account_status;
+
+        return $this;
+    }
+}
---
+++ src/Enum/UserAccountStatusEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum UserAccountStatusEnum: string
+{
+    case ACTIVE = 'active';
+    case INACTIVE = 'inactive';
+    case BLOCKED = 'blocked';
+    case BAN = 'ban';
+}
